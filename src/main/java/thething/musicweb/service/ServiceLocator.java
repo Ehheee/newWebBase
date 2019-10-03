0df562a703aa6859b953926428ab02f25cfc69d4
Kase Kaur <kaur.kase@tieto.com>
2019-10-03 11:01:37 +0300

    what?

--- src/main/java/thething/musicweb/service/ServiceLocator.java
+++ src/main/java/thething/musicweb/service/ServiceLocator.java
@@ -4,4 +4,20 @@ public class ServiceLocator {
 
 	private ServiceType serviceType;
 	private String serviceLocation;
+
+	public ServiceType getServiceType() {
+		return serviceType;
+	}
+
+	public void setServiceType(ServiceType serviceType) {
+		this.serviceType = serviceType;
+	}
+
+	public String getServiceLocation() {
+		return serviceLocation;
+	}
+
+	public void setServiceLocation(String serviceLocation) {
+		this.serviceLocation = serviceLocation;
+	}
 }
